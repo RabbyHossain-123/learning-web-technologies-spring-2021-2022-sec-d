371ef932ca676daa5df24eaf16bdbfbf7b1d1334
Rabby <rabbyhossain2265@gmail.com>
2022-02-09 10:18:28 +0600

    php file

---
+++ MID_LAB_TASK_04_PHP/Task-1.php
@@ -0,0 +1,13 @@
+<?php   
+
+$width = 5 ;   
+$length = 6 ;  
+echo "The width is $width and the length is $length ";  
+echo "<br>";
+$area = $width*$length;   
+echo "The area of a rectangle with $width x $length is $area";   
+echo "<br>";
+$perimeter = $2*($width+$length); 
+echo "The perimeter with $2x($width + $length) is $perimeter";
+
+?> 
