9dffcc3e63ecd5e6c3d06fb7524c11f048eec815
Rabby Hossain <69284462+RabbyHossain-123@users.noreply.github.com>
2022-06-16 14:26:43 +0600

    Add files via upload

---
+++ webtechlab/lab.php
@@ -0,0 +1,10 @@
+<?php
+for ($i=1; $i <=10; $i++) { 
+	$a=$i+5;
+	$cname=urlencode("Bhai amr bhai.com")
+	?>
+
+echo "<button> <a href='t.php?id= <?php echo $a;?>&cname=<?php echo $cname; ?>'>CLick here!</a> </button>"; 
+<?php
+}
+?>
